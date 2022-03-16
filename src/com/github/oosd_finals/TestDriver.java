f6d26457ac218526bed80266d7e9c33334323664
KazKB <kaleelboston@gmail.com>
2022-03-15 22:39:10 -0400

    commit

--- src/com/github/oosd_finals/TestDriver.java
+++ src/com/github/oosd_finals/TestDriver.java
@@ -7,11 +7,33 @@ public class TestDriver {
     public static boolean answer = false;
     public static String ans = "";
 
-    static void entry() {
+    static void genesis() {
         do {
             System.out.print("Do want to sign-up(s) or login(l): ");
             ans = String.valueOf(ansScanner.next().charAt(0));
         } while (!((ans.equalsIgnoreCase("s")) || (ans.equalsIgnoreCase("l"))));
+    }
+
+    static void entry() {
+        String[] signInCredentials = new String[6];
+        if (ans.equalsIgnoreCase("s")) {
+            System.out.print("Enter your first name: ");
+            signInCredentials[0] = ansScanner.next();
+            System.out.print("Enter your last name: ");
+            signInCredentials[1] = ansScanner.next();
+            System.out.print("Enter your email: ");
+            signInCredentials[2] = ansScanner.next();
+            do {
+                System.out.print("Enter your account type (admin/regular): ");
+                signInCredentials[3] = ansScanner.next();
+            } while (!((signInCredentials[3].equalsIgnoreCase("admin")) || (signInCredentials[3].equalsIgnoreCase("regular"))));
+            System.out.print("Enter your ID: ");
+            signInCredentials[4] = ansScanner.next();
+            do {
+                System.out.print("Enter your password (at least 8 characters: ");
+                signInCredentials[5] = ansScanner.next();
+            } while (signInCredentials[5].length() < 8);
+        }
     }
 
     static void exit() {
@@ -32,6 +54,8 @@ public class TestDriver {
 
     public static void main(String[] args) {
         do {
+            genesis();
+
             entry();
 
             exit();
