039d202b21d876006e8e14c78c6381f35a5ed665
KazKB <kaleelboston@gmail.com>
2022-03-17 22:46:27 -0400

    commit

--- src/com/github/oosd_finals/TestDriver.java
+++ src/com/github/oosd_finals/TestDriver.java
@@ -9,65 +9,126 @@ public class TestDriver {
     public static Scanner ansScanner = new Scanner(System.in);
     public static boolean answer = false;
     public static String ans = "";
+    public static File userFile = new File("user.txt");
+    public static User user;
 
-    static void genesis() {
+    static void genesis() throws IOException {
         do {
             System.out.print("Do want to sign-up(s) or login(l): ");
             ans = String.valueOf(ansScanner.next().charAt(0));
         } while (!((ans.equalsIgnoreCase("s")) || (ans.equalsIgnoreCase("l"))));
+
+        System.out.println();
+
+        switch (ans.toLowerCase()) {
+            case "l" -> entryLogIn();
+            case "s" -> entrySignIn();
+        }
     }
 
     static void entrySignIn() throws IOException {
         String[] signInCredentials = new String[6];
-        File user = new File("user.txt");
-
-        user.createNewFile();
-        Scanner fileReader = new Scanner(user);
+        Scanner fileReader = new Scanner(userFile);
         fileReader.useDelimiter("\r\n|,");
-        FileWriter writer = new FileWriter(user, true);
+        FileWriter writer = new FileWriter(userFile, true);
 
-        if (ans.equalsIgnoreCase("s")) {
-            System.out.print("Enter your first name: ");
-            signInCredentials[0] = ansScanner.next().trim();
-            System.out.print("Enter your last name: ");
-            signInCredentials[1] = ansScanner.next().trim();
-            System.out.print("Enter your email: ");
-            signInCredentials[2] = ansScanner.next().trim();
-            do {
-                System.out.print("Enter your account type (admin/regular): ");
-                signInCredentials[3] = String.valueOf(ansScanner.next().charAt(0));
-            } while (!((signInCredentials[3].equalsIgnoreCase("a")) || (signInCredentials[3].equalsIgnoreCase("r"))));
-            System.out.print("Enter your ID: ");
-            signInCredentials[4] = ansScanner.next().trim();
-            do {
-                System.out.print("Enter your password (at least 8 characters: ");
-                signInCredentials[5] = ansScanner.next().trim();
-            } while (signInCredentials[5].length() < 8);
+        userFile.createNewFile();
+
+        System.out.print("Enter your first name: ");
+        signInCredentials[0] = ansScanner.next().trim();
+        System.out.print("Enter your last name: ");
+        signInCredentials[1] = ansScanner.next().trim();
+        System.out.print("Enter your email: ");
+        signInCredentials[2] = ansScanner.next().trim();
+
+        do {
+            System.out.print("Enter your account type (admin/regular): ");
+            signInCredentials[3] = String.valueOf(ansScanner.next().charAt(0));
+        } while (!((signInCredentials[3].equalsIgnoreCase("a")) || (signInCredentials[3].equalsIgnoreCase("r"))));
+        System.out.print("Enter your ID: ");
+        signInCredentials[4] = ansScanner.next().trim();
+
+        do {
+            System.out.print("Enter your password (at least 8 characters: ");
+            signInCredentials[5] = ansScanner.next().trim();
+        } while (signInCredentials[5].length() < 8);
 
 
-            if (user.length() > 0) {
-                while (fileReader.hasNext()) {
-                    if (fileReader.next().equals(signInCredentials[4])) {
-                        entrySignIn();
+        if (userFile.length() > 0) {
+            while (fileReader.hasNext()) {
+                if (fileReader.next().equals(signInCredentials[4])) {
+                    entrySignIn();
+                }
+
+                fileReader.next();
+            }
+
+            for (int i = 0; i < signInCredentials.length-1; i++) {
+                writer.append(signInCredentials[i]).append(",");
+            }
+
+            writer.append(signInCredentials[5]);
+        }
+        else {
+            for (int i = 0; i < signInCredentials.length-1; i++) {
+                writer.append(signInCredentials[i]).append(",");
+            }
+
+            writer.append(signInCredentials[5]);
+        }
+
+        writer.append("\n");
+        writer.close();
+    }
+
+    static void entryLogIn() throws IOException {
+        Scanner fileReader = new Scanner(userFile);
+        fileReader.useDelimiter("\r\n|,");
+
+        userFile.createNewFile();
+
+        if (userFile.length() == 0) {
+            entrySignIn();
+        }
+        else {
+            String[] loginCredentials = new String[6];
+            String[] login = new String[2];
+            int i = 0;
+
+            System.out.print("Enter id: ");
+            login[0] = ansScanner.next().trim();
+            System.out.print("Enter password: ");
+            login[1] = ansScanner.next().trim();
+
+            while (fileReader.hasNext()) {
+                loginCredentials[i] = fileReader.next();
+                i++;
+
+                if (i == 6) {
+                    if ((loginCredentials[4].equals(login[0])) && (loginCredentials[5].equals(login[1]))) {
+                        user = new User(loginCredentials[3], loginCredentials[4]);
+                        break;
                     }
+                    else if (!(loginCredentials[4].equals(login[0])) && !(loginCredentials[5].equals(login[1]))) {
+                        i = 0;
+                        if (!(fileReader.hasNext())) {
+                            System.out.print("Try again(t) or Sign-up(s): ");
+                            ans = String.valueOf(ansScanner.next().charAt(0));
 
-                    fileReader.next();
+                            switch (ans.toLowerCase()) {
+                                case "t" -> {
+                                    entryLogIn();
+                                    System.out.println();
+                                }
+                                case "s" -> {
+                                    entrySignIn();
+                                    System.out.println();
+                                }
+                            }
+                        }
+                    }
                 }
-
-                for (int i = 0; i < signInCredentials.length-1; i++) {
-                    writer.append(signInCredentials[i]).append(",");
-                }
-                writer.append(signInCredentials[5]);
             }
-            else {
-                for (int i = 0; i < signInCredentials.length-1; i++) {
-                    writer.append(signInCredentials[i]).append(",");
-                }
-                writer.append(signInCredentials[5]);
-            }
-
-            writer.append("\n");
-            writer.close();
         }
     }
 
@@ -90,9 +151,6 @@ public class TestDriver {
     public static void main(String[] args) throws IOException {
         do {
             genesis();
-            System.out.println();
-
-            entrySignIn();
             System.out.println();
 
             exit();
